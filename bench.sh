d16b49d6fded6e4fea1edbf314315bef4da35968
Haisheng Chen <hac048@ucsd.edu>
2025-10-21 02:47:31 +0000

    fix

--- bench.sh
+++ bench.sh
@@ -1,9 +1,9 @@
-python interactive_gen.py --hf_path  meta-llama/Meta-Llama-3-8B   --bench_model --empty_model
-python interactive_gen.py --hf_path meta-llama/Meta-Llama-3-70B  --bench_model --empty_model
-python interactive_gen.py --hf_path  meta-llama/Llama-2-7b-hf  --bench_model --empty_model
-python interactive_gen.py --hf_path  meta-llama/Llama-2-13b-hf   --bench_model --empty_model
-python interactive_gen.py --hf_path  Qwen/Qwen3-1.7B   --bench_model --empty_model
-python interactive_gen.py --hf_path  Qwen/Qwen3-4B   --bench_model --empty_model
-python interactive_gen.py --hf_path  Qwen/Qwen3-8B   --bench_model --empty_model
-python interactive_gen.py --hf_path  Qwen/Qwen3-14B   --bench_model --empty_model
-python interactive_gen.py --hf_path  Qwen/Qwen3-32B   --bench_model --empty_model
+python interactive_gen.py --hf_path  relaxml/Llama-3.1-8b-Instruct-QTIP-4Bit  --bench_model 
+python interactive_gen.py --hf_path relaxml/Llama-3.3-70B-Instruct-QTIP-4Bit  --bench_model 
+python interactive_gen.py --hf_path  relaxml/Llama-2-7b-QTIP-4Bit  --bench_model 
+python interactive_gen.py --hf_path  relaxml/Llama-2-13b-QTIP-4Bit   --bench_model 
+# python interactive_gen.py --hf_path  Qwen/Qwen3-1.7B   --bench_model --empty_model
+# python interactive_gen.py --hf_path  Qwen/Qwen3-4B   --bench_model --empty_model
+# python interactive_gen.py --hf_path  Qwen/Qwen3-8B   --bench_model --empty_model
+# python interactive_gen.py --hf_path  Qwen/Qwen3-14B   --bench_model --empty_model
+# python interactive_gen.py --hf_path  Qwen/Qwen3-32B   --bench_model --empty_model
